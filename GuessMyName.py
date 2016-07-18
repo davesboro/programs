51d1666bca8df250201477c427f90af2678a8834
Alexander Sedgwick <davesboro@gmail.com>
2016-07-17 22:44:59 -0500

    Add files via upload
    I looked on the internet and didn't see another name guessing game, so I made one.

---
+++ GuessMyName.py
@@ -0,0 +1,11 @@
+answer = 'alex'
+print("Tip: Use all lower case letters")
+guess = input("Guess my name: ")
+
+if (guess == answer):
+    print("That is correct")
+    
+else:
+    print("That is incorrect. Better luck next time.")
+
+
